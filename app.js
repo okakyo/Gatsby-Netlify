1b9f5515536199da971195b6e538f1868c216e35
Kyohei Oka <0622okakyo@gmail.com>
2018-12-24 16:35:34 +0900

    '練習中'

---
+++ app.js
@@ -0,0 +1,13 @@
+const electron=require('electron');
+const app=electron.app;
+const BrowserWindow =electron.BrowserWindow;
+
+let win;
+
+app.on('ready',function(){
+    mainWindow= new BrowserWindow({width:600,height:400});
+    mainWindow.loadURL("file://"+__dirname+"/index.html");
+    mainWindow.on('closed',function(){
+        mainWindow=null;
+    });
+});
