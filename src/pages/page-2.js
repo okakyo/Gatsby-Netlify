fe8c445831d6ef0f016a7c897be4bf870fcd2d01
OkaKyo <0622okakyo@gmail.com>
2020-02-20 00:36:26 +0900

    確認

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+// 使い方後で要確認
+const SecondPage   = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
